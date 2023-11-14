b0d44acf516cfff737d600bd2bc2f6ea776f261f
anyelodiazm <anyelo.diazm@perficient.com>
2023-11-14 15:57:33 -0500

    Borrado clases que no se utilizan

--- order-app/src/main/java/com/perficient/orderapp/domain/excepton/DeadLineExceededException.java
+++
@@ -1,8 +0,0 @@
-package com.perficient.orderapp.domain.excepton;
-
-
-public class DeadLineExceededException extends RuntimeException {
-
-  public DeadLineExceededException() {
-  }
-}
--- order-app/src/main/java/com/perficient/orderapp/domain/excepton/PaymentException.java
+++
@@ -1,8 +0,0 @@
-package com.perficient.orderapp.domain.excepton;
-
-public class UnavailablePaymentException extends RuntimeException {
-
-    public UnavailablePaymentException() {
-        super("Payment Application is not available, contact Payment Support Team");
-    }
-}
